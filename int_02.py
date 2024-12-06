2b44da3da65fe350f7e2c7c6ec4af88ab4020d13
Luiz Gustavo <lgds.demtrio@gmail.com>
2024-12-06 01:09:46 -0300

    segundo exercicio feito, eu sou um maximo

---
+++ int_02.py
@@ -0,0 +1,7 @@
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero_usuario = int(input("Inserir o numero para descobrir o resto da divisao por 5: "))
+
+resto_da_divisao = (numero_usuario % 5)
+
+print(f"O resto da divisao e: {resto_da_divisao}")
