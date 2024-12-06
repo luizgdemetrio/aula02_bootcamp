28847487df18f9d7245df8cb73973e41beaa1c0d
Luiz Gustavo <lgds.demtrio@gmail.com>
2024-12-06 01:18:38 -0300

    Terceiro exercicio de inteiros resolvido sem muitos problemas

---
+++ int_03.py
@@ -0,0 +1,8 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero1 = int(input("Informe o primeiro numero: "))
+numero2 = int(input("Informe o segundo numero: "))
+
+resultado_multiplicacao = numero1 * numero2
+
+print(f"o produto da multiplicacao e {resultado_multiplicacao}")
