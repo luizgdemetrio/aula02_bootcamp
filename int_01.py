faa0b987541c7221a64e25fc352f27bbc2e6e04c
Luiz Gustavo <lgds.demtrio@gmail.com>
2024-12-06 00:36:08 -0300

    Primeiro exercicio resolvido e correto

---
+++ int_01.py
@@ -0,0 +1,8 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero1 = int(input("Informe o primeiro numero: "))
+numero2 = int(input("Informe o segundo numero: "))
+
+soma = numero1 + numero2
+
+print(f"O total da some e: {soma}")
