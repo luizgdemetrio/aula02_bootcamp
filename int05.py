b16e2202b069ab73a79e10be9f6cde2e1eeff246
Luiz Gustavo <lgds.demtrio@gmail.com>
2024-12-06 01:29:27 -0300

    realizado o quinto exercicio de boa

---
+++ int05.py
@@ -0,0 +1,7 @@
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+numero = int(input("Insira o numero para ser calculado o quadrado: "))
+
+resultado_quadrado = int(numero ** 2)
+
+print(f"O resultado e: {resultado_quadrado}")
