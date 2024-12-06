3d3a85e3aa04447de72ee79aea43b693e95e1d5d
Luiz Gustavo <lgds.demtrio@gmail.com>
2024-12-06 01:24:51 -0300

    Vou alterar o nome dos arquivos pq ficar escrevendo _ e muito chato, resolvi sem problemas

---
+++ int04.py
@@ -0,0 +1,9 @@
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero1 = int(input("Insira o primeiro numero inteiro: "))
+numero2 = int(input("Insira o segundo numero inteiro: "))
+
+divisao = numero1 // numero2
+
+print(f"O resultado da divisao por inteiros e: {divisao}")
+
