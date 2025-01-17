6bb537df4169b697cf8fe5d25f7bac6e888056f7
Luiz Gustavo <lgds.demtrio@gmail.com>
2025-01-17 01:41:45 -0300

    :sparkles: feat: Exercício finalizado

---
+++ float2.py
@@ -0,0 +1,11 @@
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+num1 = float(input("Inserir primeiro número: "))
+num2 = float(input("Inserir segundo número: "))
+
+median = (num1 + num2) / 2
+
+print(f"A media dos números {num1} e {num2} é: {median}")
+
+
+
