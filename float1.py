bd5949bd2338750dea3553a6e3c6cf72d02a0405
Luiz Gustavo <lgds.demtrio@gmail.com>
2025-01-17 01:33:27 -0300

    :sparkles: feat: Exercício de float feio

---
+++ float1.py
@@ -0,0 +1,8 @@
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+num1 = float(input("Inserir o primerio número:  "))
+num2 = float(input("Inserir o segundo número: "))
+
+soma = num1 + num2
+
+print(soma)
